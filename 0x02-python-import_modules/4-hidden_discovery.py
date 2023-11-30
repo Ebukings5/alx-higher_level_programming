95ccae5fdb0e3cf615d39a1e4085486509e16236
Ebukings5 <cristofarokoye@gmail.com>
2023-11-30 21:26:10 +0100

    Hidden names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
 
-    names = dir(hidden_4)
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
+
     for name in names:
-        if name[:2] != "__":
-            print(name)
+        print(name)
